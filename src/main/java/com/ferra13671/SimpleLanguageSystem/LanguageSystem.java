3136a04f2420c9fdd78bcf93c48f3be3ef20cdac
Ferra13671 <ferra13671@gmail.com>
2025-07-11 22:08:07 +0300

    1.2.6

--- src/main/java/com/ferra13671/SimpleLanguageSystem/LanguageSystem.java
+++ src/main/java/com/ferra13671/SimpleLanguageSystem/LanguageSystem.java
@@ -7,15 +7,20 @@ import java.util.function.Supplier;
 public class LanguageSystem {
     private static final HashMap<String, Language> languages = new HashMap<>();
     private static Supplier<String> languageGetter = null;
+    private static String defaultLanguage = "";
 
     public static void setLanguageGetter(Supplier<String> languageGetter) {
         LanguageSystem.languageGetter = languageGetter;
     }
 
+    public static void setDefaultLanguage(String defaultLanguage) {
+        LanguageSystem.defaultLanguage = defaultLanguage;
+    }
+
     public static String translate(String id) {
         if (languageGetter == null)
             throw new UnsupportedOperationException("Language getter has not initialized!");
-        return getOrCreateLanguage(languageGetter.get()).get(id);
+        return getLanguageOrDefault(languageGetter.get(), defaultLanguage).get(id);
     }
 
     public static String translate(String id, String... args) {
@@ -25,6 +30,15 @@ public class LanguageSystem {
     public static void addTranslations(String languageName, HashMap<InputStream, String> languageMap) {
         Language language = getOrCreateLanguage(languageName);
         language.addTranslations(languageMap);
+    }
+
+    public static Language getLanguage(String languageName) {
+        return languages.get(languageName);
+    }
+
+    public static Language getLanguageOrDefault(String languageName, String defaultLanguageName) {
+        Language language = getLanguage(languageName);
+        return language == null ? getOrCreateLanguage(defaultLanguageName) : language;
     }
 
     public static Language getOrCreateLanguage(String languageName) {

50a8b852a284f07fe265deb65e67eaa4e7ba09c1
Ferra13671 <ferra13671@gmail.com>
2025-07-12 05:22:38 +0300

    1.2.7

--- src/main/java/com/ferra13671/SimpleLanguageSystem/LanguageUtils.java
+++ src/main/java/com/ferra13671/SimpleLanguageSystem/LanguageUtils.java
@@ -6,6 +6,7 @@ import com.google.gson.JsonPrimitive;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
 public class LanguageUtils {
@@ -15,7 +16,7 @@ public class LanguageUtils {
     }
 
     protected static HashMap<String, TextContainer> loadFromInputStream(InputStream inputStream) {
-        JsonObject jsonObject = JsonParser.parseReader(new InputStreamReader(inputStream)).getAsJsonObject();
+        JsonObject jsonObject = JsonParser.parseReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8)).getAsJsonObject();
 
         HashMap<String, TextContainer> languageMap = new HashMap<>();
 
